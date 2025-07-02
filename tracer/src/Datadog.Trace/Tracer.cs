DataDog/dd-trace-dotnet

WIP: Refactor debugger features to support configuration updates

tracer/src/Datadog.Trace/Tracer.cs
@@ -12,6 +12,7 @@ using Datadog.Trace.Agent;
 using Datadog.Trace.Agent.DiscoveryService;
 using Datadog.Trace.ClrProfiler;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.Debugger;
 using Datadog.Trace.Debugger.SpanCodeOrigin;
 using Datadog.Trace.Logging.TracerFlare;
 using Datadog.Trace.Sampling;
@@ -526,7 +527,7 @@ namespace Datadog.Trace
             // write them directly to the <see cref="TraceChunkModel"/>.
             TracerManager.GitMetadataTagsProvider.TryExtractGitMetadata(out _);
 
-            SpanCodeOrigin.Instance.SetCodeOrigin(span);
+            DebuggerManager.Instance.CodeOrigin?.SetCodeOrigin(span);
 
             return span;
         }

dineshexa/Dot-Net-Test

tracer/src/Datadog.Trace/Tracer.cs
@@ -13,6 +13,7 @@ using Datadog.Trace.Agent;
 using Datadog.Trace.Agent.DiscoveryService;
 using Datadog.Trace.ClrProfiler;
 using Datadog.Trace.Configuration;
+using Datadog.Trace.Debugger;
 using Datadog.Trace.Debugger.SpanCodeOrigin;
 using Datadog.Trace.Logging.TracerFlare;
 using Datadog.Trace.Sampling;
@@ -528,7 +529,7 @@ namespace Datadog.Trace
             // write them directly to the <see cref="TraceChunkModel"/>.
             TracerManager.GitMetadataTagsProvider.TryExtractGitMetadata(out _);
 
-            SpanCodeOrigin.Instance.SetCodeOriginForExitSpan(span);
+            DebuggerManager.Instance.CodeOrigin?.SetCodeOriginForExitSpan(span);
 
             return span;
         }